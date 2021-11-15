2d36ef92d1bf02abdf29af6e1a872678e064b78b
Anand <anandnatarajanbtech@gmail.com>
2021-11-15 13:39:08 +0530

    create product entity

---
+++ src/main/java/com/app/sampleproject/entity/Product.java
@@ -0,0 +1,82 @@
+package com.app.sampleproject.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name ="product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int productid;
+    private String productname;
+    private String productcode;
+    private int price;
+    private double offer;
+    private String description;
+    private String color;
+    private String date;
+
+    public int getProductid() {
+        return productid;
+    }
+
+    public void setProductid(int productid) {
+        this.productid = productid;
+    }
+
+    public String getProductname() {
+        return productname;
+    }
+
+    public void setProductname(String productname) {
+        this.productname = productname;
+    }
+
+    public String getProductcode() {
+        return productcode;
+    }
+
+    public void setProductcode(String productcode) {
+        this.productcode = productcode;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public double getOffer() {
+        return offer;
+    }
+
+    public void setOffer(double offer) {
+        this.offer = offer;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
